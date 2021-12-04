430391de4460651d152e3c50694fd0ded81aec2e
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2021-12-04 13:53:21 -0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,6 @@
 <?php
-$con  = mysqli_connect('localhost','root','','titan');
+// $con  = mysqli_connect('localhost','root','','titan');
+$con  = mysqli_connect("us-cdbr-east-04.cleardb.com", "ba932adfb213b7", "7ee70427", "heroku_573ed5910b2ee50");
 if(mysqli_connect_errno())
 {
     echo 'Database Connection Error';
