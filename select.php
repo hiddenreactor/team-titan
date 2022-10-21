be211f4e892087ef7f833fa9708a058dcbe274a6
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2022-10-21 16:06:18 -0700

    Update select.php

--- select.php
+++ select.php
@@ -2,7 +2,7 @@
 
 // include('config_BD.php');
 // $connect = new PDO("mysql:host=localhost; dbname=titan", "root", "");
-$connect = new PDO('mysql:host=us-cdbr-east-04.cleardb.com; dbname=heroku_22c64d3e4214282;', 'b4a111629544d9', '059962b1');
+$connect = new PDO("mysql:host=us-cdbr-east-04.cleardb.com; dbname=heroku_22c64d3e4214282;", "b4a111629544d9", "059962b1");
 if(isset($_POST["playerID"]))
 {
 
