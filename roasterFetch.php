43b527e3ccffb6124d8afadbf26830c7d84f0e65
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2022-10-20 21:55:12 -0700

    Update roasterFetch.php

--- roasterFetch.php
+++ roasterFetch.php
@@ -1,6 +1,7 @@
 <?php
 //fetch.php
-$connect = mysqli_connect("localhost", "root", "", "titan");
+// $connect = mysqli_connect("localhost", "root", "", "titan");
+$connect = mysqli_connect("us-cdbr-east-04.cleardb.com", "b4a111629544d9", "059962b1", "heroku_22c64d3e4214282");
 $columns = array('playerID', 'playerName', 'teamID', 'groupID', 'yearID');   //this has to match with the db
 
 // $query = "SELECT * FROM inventory ";
