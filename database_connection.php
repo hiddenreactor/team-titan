41ddde07ec52d7a610cae9f9f529457b81adb21f
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2022-10-21 16:55:52 -0700

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,7 @@
 <?php
 
-$username = 'ba932adfb213b7';
-$password = '7ee70427';
-$connection = new PDO("mysql:host=us-cdbr-east-04.cleardb.com; dbname=heroku_573ed5910b2ee50;", "ba932adfb213b7", "7ee70427");
+// $username = 'ba932adfb213b7';
+// $password = '7ee70427';
+$connection = new PDO("mysql:host=us-cdbr-east-04.cleardb.com; dbname=heroku_22c64d3e4214282", "b4a111629544d9", "059962b1");
 
 ?>
