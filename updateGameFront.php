465aa6ba15462aafaa6a7e293a226325e797d2cd
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2022-10-21 16:54:55 -0700

    Update updateGameFront.php

--- updateGameFront.php
+++ updateGameFront.php
@@ -1,7 +1,8 @@
 
  <?php  
  //fetch.php  
- $connect = mysqli_connect("localhost", "root", "", "titan");  
+//  $connect = mysqli_connect("localhost", "root", "", "titan");  
+ $connect = mysqli_connect("us-cdbr-east-04.cleardb.com", "b4a111629544d9", "059962b1", "heroku_22c64d3e4214282");
  if(isset($_POST["user_id"]))  
  {  
     //   $query = "SELECT * FROM gamestat WHERE gamestat.gameID = '".$_POST["user_id"]."'"; 
