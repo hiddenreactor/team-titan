6821c07acbf1d905f1a6b8f98e1453c1d2a5a875
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2022-10-21 17:08:41 -0700

    Update config_BD.php

--- config_BD.php
+++ config_BD.php
@@ -1,16 +1,6 @@
 <?php
 
-// $host = "us-cdbr-east-04.cleardb.com"; /* Host name */
-// $user = "ba932adfb213b7"; /* User */
-// $password = "7ee70427"; /* Password */
-// $dbname = "heroku_573ed5910b2ee50"; /* Database name */
-
-$host = "us-cdbr-east-04.cleardb.com"; /* Host name */
-$user = "b4a111629544d9"; /* User */
-$password = "059962b1"; /* Password */
-$dbname = "heroku_22c64d3e4214282"; /* Database name */
-
-$con = mysqli_connect($host, $user, $password, $dbname);
+$con = mysqli_connect("us-cdbr-east-04.cleardb.com", "b4a111629544d9", "059962b1", "heroku_22c64d3e4214282");
 // Check connection
 if (!$con) {
  	die("Connection failed: " . mysqli_connect_error());
