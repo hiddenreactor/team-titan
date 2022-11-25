77433ecf17a07d4832b18900d63169211cd965b5
hiddenreactor <51773244+hiddenreactor@users.noreply.github.com>
2022-11-24 21:26:13 -0800

    Update allrecord.php

--- allrecord.php
+++ allrecord.php
@@ -324,7 +324,7 @@ input[type=number] {
                             </td> -->
                           <td>
                             <div class="quantity" id='playtime_<?php echo $id; ?>'>
-                              <input type="text" id='playtime_<?php echo $id; ?>' value='<?php echo $playtime; ?>' readonly="readonly">
+                              <input type="text" id='playtime_<?php echo $id; ?>' value='<?php echo $playtime; ?>' style="width: 10%" readonly="readonly">
                             </div>
                           </td>                         
                       </tr>
